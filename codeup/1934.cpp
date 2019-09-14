ec21e8c999ed2a2a3dca3dfee94117c373c12fc0
Loner1024 <xth12138@gmail.com>
2019-09-14 22:02:05 +0800

    找x

--- codeup/1934.cpp
+++ codeup/1934.cpp
@@ -15,3 +15,32 @@
 样例输出
 2
 */
+
+#include <cstdio>
+
+int main(){
+    int a[201];
+    int n, x;
+    while (scanf("%d", &n) != EOF)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &a[i]);
+        } 
+        scanf("%d", &x);
+        int k;
+        for (k = 0; k < n; k++)
+        {
+            if (a[k] == x)
+            {
+                printf("%d\n", k);
+                break;
+            }
+        }
+        if (k == n)
+        {
+            printf("-1\n");
+        } 
+    }
+    return 0;
+}
